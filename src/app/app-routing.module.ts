6eac141aaf93f4c1327054bc777396f0bacb12f1
Nahla Gamal <nahladayya@gmail.com>
2023-04-20 17:29:54 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './Components/user-login/user-login.component';
-import { RegisterComponent } from './Components/register/register.component';
-import { HomeComponent } from './Components/Core/home/home.component';import { MainlayoutComponent } from './mainlayout/mainlayout.component';
-import { HomeComponent } from './Core/home/home.component';
+import { MainlayoutComponent } from './mainlayout/mainlayout.component';
+import { HomeComponent } from './Components/Core/home/home.component';
 import { ProductDetailsComponent } from 'src/app/Components/orders/product-details/product-details.component';
 import { ProductListComponent } from 'src/app/Components/orders/product-list/product-list.component';
 import { OrdersComponent } from 'src/app/Components/orders/orders.component';
 import { NotFoundError } from 'rxjs';
+import { RegisterComponent } from './Components/register/register.component';
 
 const routes: Routes = [
-  { path: 'login', component: UserLoginComponent },
-];
+    {path:'',component:MainlayoutComponent,children:[
+      {path:'Home',component:HomeComponent},
+      {path:'products/:id',component:ProductDetailsComponent},
+      {path:'Products',component:ProductListComponent},
+      {path:'Order',component:OrdersComponent},
+      {path:'' ,redirectTo:'/Home',pathMatch:'full'},
+    ]},
+    {path:'LogIn',component:UserLoginComponent},
+    { path: 'register', component: RegisterComponent },
+    {path:'**',component:NotFoundError}
+  ];
+;
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
